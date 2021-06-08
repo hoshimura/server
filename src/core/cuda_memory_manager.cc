fb547da51cc6dbeb89f4adced1e7806811e3a828
CoderHam <hemanham@gmail.com>
2021-06-08 14:41:20 -0700

    Fix cuda memory manager destructor when not initialized - cnmem is not initialized when memory pool sizes are 0

--- src/core/cuda_memory_manager.cc
+++ src/core/cuda_memory_manager.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+// Copyright (c) 2020-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
@@ -61,10 +61,12 @@ std::mutex CudaMemoryManager::instance_mu_;
 
 CudaMemoryManager::~CudaMemoryManager()
 {
-  auto status = cnmemFinalize();
-  if (status != CNMEM_STATUS_SUCCESS) {
-    LOG_ERROR << "Failed to finalize CUDA memory manager: [" << status << "] "
-              << cnmemGetErrorString(status);
+  if (has_allocation_) {
+    auto status = cnmemFinalize();
+    if (status != CNMEM_STATUS_SUCCESS) {
+      LOG_ERROR << "Failed to finalize CUDA memory manager: [" << status << "] "
+                << cnmemGetErrorString(status);
+    }
   }
 }
 
